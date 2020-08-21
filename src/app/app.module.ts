709f97aed53410b3cd0c0a19379910e59e6b3bf1
Steve Arditti <steven.arditti@gmail.com>
2020-08-21 16:03:19 -0400

    init commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FeatureTestComponent } from './feature1/feature-test.component';
+
+@NgModule({
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        FeatureTestComponent
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class AppModule { }
---
+++ src/app/feature1/feature-test.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'feature-test',
+    template: '<Label text="ayo" row="1" col="1"></Label>'
+})
+
+export class FeatureTestComponent implements OnInit {
+
+    constructor() {
+
+    }
+
+    ngOnInit() {
+    }
+
+}
